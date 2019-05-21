65ca808ea5d4b881a27c64bee5155ec6835366e0
Anton <tony_kovalenko@mail.ru>
2019-05-21 21:29:11 +0700

    Initial Commit

---
+++ AR Laboratory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR Laboratory
+//
+//  Created by Anton Kovalenko on 21/05/2019.
+//  Copyright © 2019 Anton Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
